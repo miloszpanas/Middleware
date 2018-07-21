e20cc3198ac5180dd89aa5affa802c8f76e8d01c
Miłosz Panas <miloszpanas@gmail.com>
2018-07-21 17:21:50 +0200

    Add files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+app.use(function (req, res, next) {
+    console.log('Hej, jestem pośrednikiem przy żądaniu do .store!');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.get('/store', function(req, res) {
+    res.send('To jest sklep');
+});
+
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
